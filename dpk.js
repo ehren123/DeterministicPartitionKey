e764442731d620e479b1d0c2147a49caee79c009
Ehren Lillies <ehren.lillies@nexusinno.com>
2023-03-06 19:16:32 -0500

    code cleanup

--- dpk.js
+++ dpk.js
@@ -11,21 +11,21 @@ function deterministicPartitionKey(event) {
 
   let partitionKey;
 
-  // If the event object is defined and has a partition key, use it as the candidate
+  // If the event object is defined and has a partition key, use it as the candidate.
   if (event.partitionKey) {
     partitionKey = event.partitionKey;
   } else {
-    // Otherwise, hash the event object using SHA3-512 and use the digest as the candidate
+    // Otherwise, hash the event object using SHA3-512 and use the digest as the candidate.
     const data = JSON.stringify(event);
     partitionKey = crypto.createHash('sha3-512').update(data).digest('hex');
   }
 
-  // Convert the candidate to a string if it isn't already one
+  // Convert the candidate to a string if it isn't already one.
   if (typeof partitionKey !== 'string') {
     partitionKey = JSON.stringify(partitionKey);
   }
 
-  // If the candidate is too long, hash it again using SHA3-512
+  // If the candidate is too long, hash it again using SHA3-512.
   if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
     partitionKey = crypto.createHash('sha3-512').update(partitionKey).digest('hex');
   }
