4c56e700c9caf4e03b2a27dbb327f50066aaa65a
Ehren Lillies <ehren.lillies@nexusinno.com>
2023-03-06 19:20:06 -0500

    code cleanup

--- dpk.test.js
+++ dpk.test.js
@@ -35,7 +35,7 @@ describe('deterministicPartitionKey', () => {
     expect(deterministicPartitionKey(event)).toEqual(expected);
   });
 
-  it('Hash a partition key above 256 characters', () => {
+  it('Hashes a partition key above 256 characters', () => {
     const event = { partitionKey: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida lobortis tortor, quis placerat mi elementum vel. Duis condimentum finibus metus ac venenatis. Praesent tincidunt felis maximus pulvinar ultrices. In suscipit quam non libero pretium rutrum. Vivamus tristique enim sed enim consectetur rutrum. Mauris vel urna fringilla, interdum leo ullamcorper, faucibus felis. Aenean dapibus dui quis mi ultrices volutpat. Etiam elit eros, tincidunt eget dignissim sit amet, sollicitudin a lacus. Mauris non pharetra elit. Mauris non nunc eu elit consequat pharetra quis a felis. Nulla sodales dignissim augue, vitae blandit nibh semper vel. Vivamus nisi risus, placerat a mi quis, feugiat aliquam dolor. Donec consectetur efficitur magna nec tempor. Donec imperdiet libero et risus malesuada, sit amet vehicula elit dictum. Nunc varius ante eu urna vulputate vehicula. Quisque ut molestie est, eget vehicula quam.' };
     const expected = '63f6a6312de2cb107f2bbc018d486a85c87c81ea8fbda024f74096eb0a62b9bfee73fe1f9a06231104c3b98119aaa6764dc75c3ce2397f797b240b55f0d06174';
     expect(deterministicPartitionKey(event)).toEqual(expected);
