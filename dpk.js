897b7df241d4ecd46461d94afcef0a5ff39ce1b3
Ehren Lillies <ehren.lillies@nexusinno.com>
2023-03-06 19:04:45 -0500

    code cleanup

--- dpk.js
+++ dpk.js
@@ -9,28 +9,28 @@ function deterministicPartitionKey(event) {
     return TRIVIAL_PARTITION_KEY;
   }
 
-  let candidate;
+  let partitionKey;
 
   // If the event object is defined and has a partition key, use it as the candidate
   if (event.partitionKey) {
-    candidate = event.partitionKey;
+    partitionKey = event.partitionKey;
   } else {
     // Otherwise, hash the event object using SHA3-512 and use the digest as the candidate
     const data = JSON.stringify(event);
-    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+    partitionKey = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
   // Convert the candidate to a string if it isn't already one
-  if (typeof candidate !== "string") {
-    candidate = JSON.stringify(candidate);
+  if (typeof partitionKey !== "string") {
+    partitionKey = JSON.stringify(partitionKey);
   }
 
   // If the candidate is too long, hash it again using SHA3-512
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    partitionKey = crypto.createHash("sha3-512").update(partitionKey).digest("hex");
   }
 
-  return candidate
+  return partitionKey
 };
 
 module.exports = { deterministicPartitionKey };
